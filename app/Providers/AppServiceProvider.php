33b624be0b2f6977acdb41e2c0fdc2c503755213
Tiemsky <tiafranck31@yahoo.fr>
2026-03-26 17:39:37 +0000

    Ready For Prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -19,9 +20,12 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+     public function boot(): void
     {
+      if (app()->environment('production')) {
+           URL::forceScheme('https');
+      }
         Vite::prefetch(concurrency: 3);
-        JsonResource::withoutWrapping();
+         JsonResource::withoutWrapping();
     }
 }
