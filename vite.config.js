647ae02f1ed2cdede55c6731313861579e638c04
Tiemsky <tiafranck31@yahoo.fr>
2026-03-26 16:35:28 +0000

    Fix: Asset

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  server: {
-    // Indispensable pour Docker : écouter sur toutes les interfaces
-    host: '0.0.0.0',
-    hmr: {
-      // Indispensable pour que ton navigateur puisse se connecter au flux HMR
-      host: 'localhost',
-    },
-    // S'assurer que le port correspond à celui déclaré dans ton docker-compose
-    port: 5173,
-    watch: {
-      usePolling: true, // Parfois nécessaire si les changements de fichiers ne sont pas détectés sur Windows/WSL
-    },
-  },
   plugins: [
     laravel({
       input: 'resources/js/app.js',
